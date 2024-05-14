dac0f83aa3841dc2e3aaf5424bae2de9fa50217e
Benjamin Kaiser <bekaise@microsoft.com>
2024-05-14 22:20:50 +1000

    Add House of Miracles

--- run-windows.sh
+++ run-windows.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-/c/Program\ Files/ChordPro.ORG/ChordPro/chordpro.exe "Abbie Gamboa-Pure.cho" --config=chordpro.json
+# Define the path to the ChordPro executable
+CHORDPRO_EXEC="/c/Program Files/ChordPro.ORG/ChordPro/chordpro.exe"
+
+# Define the path to the configuration file
+CONFIG_FILE="chordpro.json"
+
+# Loop through each .cho file in the current folder
+for FILE in *.cho;
+do
+    # Run the ChordPro command on each file
+    "$CHORDPRO_EXEC" "$FILE" --config="$CONFIG_FILE"
+done
